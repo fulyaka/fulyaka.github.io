241e0c99c4e26415ed65db00bdb0855a96abb955
fulyaka <49833610+fulyaka@users.noreply.github.com>
2020-09-30 15:10:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,34 @@
-            var button = document.getElementById("countButton");	//кнопка для отправки кол-ва 
-			var numberArray = document.getElementsByClassName("number"); 
-    		var rowNumbers = document.getElementById("row_numbers");	//область для вывода массива
-    		var count = document.getElementById("countInput");	//ввод числа (кол-во элементов массива)
-    		var areaInputs = document.getElementById("row_inputs"); //область для вывода input-ов элементов
-			button.onclick = printInputs; 
-			
-			//Функция выводит поля для ввода, предварительно спросив количество чисел в массиве (с проверкой на правильность)
-			function printInputs() {
-				areaInputs.innerHTML='';
-				rowNumbers.innerHTML='';
-				if(isNaN(count.value) || (count.value<=0) || (count.value%1>0)) {
-						areaInputs.innerHTML="<span class='attention'>Некорректный ввод! Введите количество чисел в массиве (целое положительное число)</span>"
-					}
-				else {
-						for (i=0; i<count.value; i++) {
-							areaInputs.innerHTML+='<input type="text" size="3" maxlength="3" class="number input">';
-						}
-						areaInputs.innerHTML+='<br><input type="button" value="Ввести массив" id="arrayButton"><a href="index.htm"><input type="button" value="Новый массив" id="new"></a>';
-						var arrayButton = document.getElementById("arrayButton");
-						var form = document.getElementById("forma");
-    					form.style.display = 'none';
-    					header.textContent = "Введите целочисленные элементы массива размером "+Math.round(count.value);
-						arrayButton.onclick = obrabotka;
-				}
-    		};
-    		//функция, обрабатывающая содержимое полей и переносящая в массив валидные данные
-    		function obrabotka() {
-    			var flag=true;
-    			var arr = new Array(parseInt(count.value));
-    			for (var i=0; i<arr.length; i++) {
-    				if(!isNaN(numberArray[i].value) && (numberArray[i].value%1==0) ) {
-    					arr[i] = Number.parseInt(numberArray[i].value);
-    				}
-    				findIndMax(arr);
-    			}
+// Some code thanks to @chrisgannon
 
-    			
-    		}
+var select = function(s) {
+  return document.querySelector(s);
+}
 
-    		//функция для тестового задания 
-    		function findIndMax(arr) {
-    			var max = arr[0];
-    			var flag=true;
-    			var indMax=0;
-    			 		for (var i = 0; i < arr.length; i++) {
-							if(isNaN(arr[i]) || (arr[i]%1>0)) {
-								flag=false;
-								break;
-							} else {
-    			 		 		if(max < arr[i]) {
-    			 		 			max =  arr[i];
-    			 		 			indMax = i;
-    			 		 		}
-    			 		 	} 
-    				 	};
-    			 	
-    			if(flag==true) {
-    				rowNumbers.innerHTML='<span class="attention success">Индекс максимального элемента, если считать от нуля, равен = '+indMax+'  '+'</span>'; //return indMax
-    			}
-    			else {
-    				rowNumbers.innerHTML='<span class="attention">Некорректный ввод элементов целочисленного массива! </span>';
-    			}
-    		};
+function randomBetween(min,max)
+{
+    var number = Math.floor(Math.random()*(max-min+1)+min);
+  
+    if ( number !== 0 ){
+      return number;
+    }else {
+      return 0.5;
+    }
+}
+
+var tl = new TimelineMax();
+
+for(var i = 0; i < 20; i++){
+
+  var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
+    x: randomBetween(12, 15) * (randomBetween(-1, 1)),
+    y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
+    repeat:-1,
+    repeatDelay:randomBetween(0.2, 0.5),
+    yoyo:true,
+    ease:Elastic.easeOut.config(1, 0.5)
+  })
+
+  tl.add(t, (i+1)/0.6)
+}
+
+tl.seek(50);
